3dc579bb4a4ba61b5f426c4ff4383c2ea831da95
Yann Diorcet <yann.diorcet@xenocs.com>
2018-04-27 16:07:16 +0200

    Improve pythran dist

--- pythran/dist.py
+++ pythran/dist.py
@@ -22,41 +22,32 @@ class PythranExtension(Extension):
     The compilation process ends up in a native Python module.
     '''
     def __init__(self, name, sources, *args, **kwargs):
-        # the goal is to rely on original Extension
-        # to do so we convert the .py to .cpp with pythran
-        # and register the .cpp in place of the .py
-        # That's stage 0, and it's enough if you get the source
-        # from github and `python setup.py install it`
-        #
-        # *But* if you want to distribute the source through
-        # `python setup.py sdist` then the .py no longer exists
-        # and only the .cpp is distributed. That's stage 1
+        kwargs.update(cfg.make_extension())
+        self._sources = sources
+        Extension.__init__(self, name, sources, *args, **kwargs)
+        self.__dict__.pop("sources", None)
 
+    @property
+    def sources(self):
         cxx_sources = []
-        for source in sources:
-            base, _ = os.path.splitext(source)
+        for source in self._sources:
+            base, ext = os.path.splitext(source)
+            if ext != '.py':
+                cxx_sources.append(source)
+                continue
             output_file = base + '.cpp'  # target name
 
-            # stage 0 when we have the .py
-            if os.path.exists(source):
-                stage = 0
-            # stage 1 otherwise. `.cpp' should already be there
-            # as generated upon stage 0
-            else:
-                assert os.path.exists(output_file)
-                stage = 1
-                source = output_file
-
-            # stage-dependant processing
-            if stage == 0:
+            if not os.path.exists(output_file) or os.stat(output_file) < os.stat(source):
                 # get the last name in the path
-                if '.' in name:
-                    module_name = os.path.splitext(name)[-1][1:]
+                if '.' in self.name:
+                    module_name = os.path.splitext(self.name)[-1][1:]
                 else:
-                    module_name = name
+                    module_name = self.name
                 tc.compile_pythranfile(source, output_file,
                                        module_name, cpponly=True)
             cxx_sources.append(output_file)
+        return cxx_sources
 
-        kwargs.update(cfg.make_extension())
-        Extension.__init__(self, name, cxx_sources, *args, **kwargs)
+    @sources.setter
+    def sources(self, sources):
+        self._sources = sources
