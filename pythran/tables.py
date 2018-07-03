2ebb44df0420646dff4d9c363b6adc078a9b542e
Yann Diorcet <yann.diorcet@xenocs.com>
2018-07-03 12:46:55 +0200

    WIP: Add unravel_index

--- pythran/tables.py
+++ pythran/tables.py
@@ -3916,6 +3916,7 @@ MODULES = {
         "union1d": ConstFunctionIntr(),
         "unique": ConstFunctionIntr(),
         "unwrap": ConstFunctionIntr(),
+        "unravel_index": ConstFunctionIntr(),
         "var": ConstMethodIntr(),
         "vstack": ConstFunctionIntr(),
         "where": ConstFunctionIntr(),
